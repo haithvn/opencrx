e332d6c1019df971cfd93b9ba786be8bd4b17544
Werner Froidevaux <wfro64@users.noreply.github.com>
2025-06-19 23:16:39 +0200

    Release opencrx-v6.0.2 #154

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,7 @@ apply(plugin = "java")
 
 allprojects {
     group = "org.opencrx"
-    version = "6.0.1"
+    version = "6.0.2"
     var env = Properties()
     env.load(FileInputStream(File(project.getRootDir(), "build.properties")))
     val targetPlatform = JavaVersion.valueOf(env.getProperty("target.platform"))    
